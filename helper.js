15a2e66ec99d33c71f10df3ada0058035629ca17
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-09-02 19:50:18 +0800

    upd

---
+++ helper.js
@@ -0,0 +1,20 @@
+export const checkCanyonFormat = (canyon) => {
+//   必须含有的字段，projectID,dsn,reporter,sha，instrumentCwd
+  if (!canyon.projectID){
+    console.log('projectID字段不存在')
+    return false
+  } else if (!canyon.dsn||!canyon.includes('http')){
+    console.log('dsn字段不存在或格式错误')
+    return false
+  } else if (!canyon.reporter){
+    console.log('reporter字段不存在')
+    return false
+  } else if (!canyon.sha||canyon.sha.length<16){
+    console.log('sha字段不存在或格式错误')
+    return false
+  } else if (!canyon.instrumentCwd){
+    console.log('instrumentCwd字段不存在')
+    return false
+  }
+  return true
+}
--- main.js
+++ main.js
@@ -1,39 +1,47 @@
 import fs from 'fs';
 import istanbulLibCoverage from "istanbul-lib-coverage";
 import axios from "axios";
+import {checkCanyonFormat} from "./helper.js";
 const initialCoverage = istanbulLibCoverage.createCoverageMap({})
 
 
-export const uploadCanyonCoverage = ()=>{
+export const uploadCanyonCoverage = async ()=>{
+  // 1.检查是否存在.canyon_output文件夹
+  if (!fs.existsSync('./.canyon_output')) {
+    console.log('.canyon_output文件夹不存在')
+      return false
+  }
+
+  // 2.检查是否存在canyon.json文件
+  if (!fs.existsSync('./.canyon_output/canyon.json')) {
+    console.log('canyon.json文件不存在')
+      return false
+  }
+  const canyon = JSON.parse(fs.readFileSync('./.canyon_output/canyon.json', 'utf-8'))
+
+  // 3.检查canyon文件格式
+
+  if (!checkCanyonFormat(canyon)) {
+    console.log('canyon文件格式错误')
+      return false
+  }
+
   const files = fs.readdirSync('./.canyon_output/')
+
+  let arr = []
 
   files.forEach(file => {
       if (file.includes('coverage-')) {
+        arr.push(file)
           initialCoverage.merge(JSON.parse(fs.readFileSync(`./.canyon_output/${file}`, 'utf-8')))
       }
   })
-  
+
+  console.log(`共发现${arr.length}个覆盖率文件`)
   fs.writeFileSync('./.canyon_output/coverage-final.json', JSON.stringify(initialCoverage.toJSON(), null, 2), 'utf-8')
-  
-  const canyon = JSON.parse(fs.readFileSync('./.canyon_output/canyon.json', 'utf-8'))
-  function newAtob() {
-    try {
-      return typeof atob === 'function' ? atob : null
-    } catch (e) {
-      return null
-    }
-  }
-  const newatob = newAtob()
-  
-  const proxy = (process.env.CI_SERVER_URL || '').includes(newatob('Y3RyaXA=')) ? {
-    proxy: {
-      protocol: 'http',
-      host: newatob('cHJveHlnYXRlMi5jdHJpcGNvcnAuY29t'),
-      port: 8080
-    }
-  } : {}
+
   try {
-    axios.post(canyon.dsn.replace('https://','http://'), {
+    await axios.post(canyon.dsn.replace('https://','http://'), {
       coverage: initialCoverage.toJSON(),
       ...canyon
     }, {
@@ -41,11 +49,14 @@ export const uploadCanyonCoverage = ()=>{
         Authorization: 'Bearer ' + canyon.reporter,
       },
       timeout: 15000,
-      ...proxy
+    }).then(res=>{
+      return true
     }).catch(err=>{
       console.log('Failed to post coverage data:', err)
+      return false
     })
   } catch (e) {
+    return false
     console.log('Failed to post coverage data:', e)
   }
 }
